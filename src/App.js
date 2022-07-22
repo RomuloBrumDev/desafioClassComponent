cf513d16510188ad489417d57efa5f8f65b50835
Romulo Brum da Silva <102628558+RomuloBrumDev@users.noreply.github.com>
2022-07-22 00:42:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// Faça login no CodeSandbox
+
+
+// Crie um novo sandbox em REACT
+
+
+// Crie um Component de class que retorne uma frase na tela
+
+
+import React class App "react"
+
+export default function App() {
+  return(
+      <h1>Olá Galera!!</h1>
+  )
+}
